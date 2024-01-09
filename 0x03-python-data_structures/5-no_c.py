af56bd14b43d0e6334d9683c6f6bfa4466c0ddd4
jakobo007 <oumajamesjoshua@gmail.com>
2024-01-09 09:27:28 +0300

    Data Structures Errors fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     if my_string[:]:
         x_string = my_string.translate({ord("c"): None})
-        y_string = my_string.translate({ord("C"): None})
+        y_string = x_string.translate({ord("C"): None})
         return y_string
     return my_string
