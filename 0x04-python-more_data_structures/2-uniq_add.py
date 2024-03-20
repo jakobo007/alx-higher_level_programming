14c183d5e2a76b67826a556f7c99f895db83d22d
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-03-20 23:52:11 +0300

    Unique Addition Changes

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     uniq_num = set()
-    for x in my_list:
-        if x not in uniq_num:
+    if my_list:
+        for x in my_list:
             uniq_num.add(x)
 
         return sum(uniq_num)
