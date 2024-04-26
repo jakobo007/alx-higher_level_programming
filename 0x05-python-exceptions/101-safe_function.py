c32efdb994c23bc24b799ad8665fb19a219b5cf3
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-04-26 21:23:49 +0300

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        func = fct(*args)
+        return func
+    except Exception as err:
+        print(f"Exception: {err}", file=sys.stderr)
+        return None
