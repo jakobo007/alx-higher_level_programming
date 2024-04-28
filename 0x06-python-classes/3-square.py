0ef1b6c4d5b81d16906ae367a2e3830cc172e72a
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-04-28 23:23:07 +0300

    object3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Define a class with private instance and a public method"""
+"""A python class with private instance"""
+
 class Square:
+    """Private instance attribute size"""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -10,4 +13,4 @@ class Square:
             self.__size = size
     
     def area(self):
-         return self.__size * self.__size
+        return (self.__size ** 2)
