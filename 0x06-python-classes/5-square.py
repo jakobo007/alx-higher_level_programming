02e6d1655ab0c9e8f90a234754202945503a4a13
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-04-28 22:43:56 +0300

    class10

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a class with private instance and a public method"""
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    @property
+    def size(self):
+        return(self.__size)
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
+    def area(self):
+         return self.__size * self.__size
+     
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print('#' * self.size)
