776234346de8cf9888ae2c62f1ebae21672b48f1
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-04-28 23:28:26 +0300

    Access and update private attribute

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,3 +14,4 @@ class Square:
     
     def area(self):
         return (self.__size ** 2)
+    
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Define a class with private instance and a public method"""
+"""A python class with private instance"""
+
 class Square:
+    """Private instance attribute size"""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -8,9 +11,14 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+    
+    def area(self):
+        return (self.__size ** 2)
+    
     @property
     def size(self):
         return(self.__size)
+    
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -19,6 +27,4 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
-    def area(self):
-         return self.__size * self.__size
+            
