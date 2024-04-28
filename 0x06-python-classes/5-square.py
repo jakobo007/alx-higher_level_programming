aac30f1f5ca20929a4c284698e89d0c0291d3ee6
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-04-28 23:37:12 +0300

    object5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,20 +5,12 @@ class Square:
     """Private instance attribute size"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-    
-    def area(self):
-        return (self.__size ** 2)
-    
+        self.size = size
+
     @property
     def size(self):
-        return(self.__size)
-    
+        return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -27,11 +19,15 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
+    def area(self):
+        return self.size * self.size
+
     def my_print(self):
         if self.size == 0:
             print()
         else:
             for _ in range(self.size):
-                print('#', self.size)
+                print('#' * self.size)
+
         
