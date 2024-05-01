28009d7693899c044b773e7b130fd4eaa1ee2212
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-05-01 13:52:26 +0300

    docstrings

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,6 +9,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
     
     if last_name:
-        return f'My name is {first_name} {last_name}'
+        return f"My name is {first_name} {last_name}"
     else:
-        return f'My name is {first_name}'
+        return f"My name is {first_name}"
