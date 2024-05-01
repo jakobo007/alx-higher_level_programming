be8b416b4d1f12288e49f457846fd19e141a07c4
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-05-01 10:06:04 +0300

    integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module 0-add_integer
+function that adds 2 integers
+"""
 def add_integer(a, b=98):
     if not isinstance(a,(int, float)):
         raise TypeError("a must be an integer")
