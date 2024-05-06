09b366458371ec90fcc10c21842d6e22e0d3dc6e
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-05-06 10:33:43 +0300

    Changes2

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,7 +3,9 @@
 import json
 """A function that returns the JSON of an object(string)"""
 
+
 def to_json_string(my_obj):
 
-    """obj is the object to convert"""
+    """obj is the object to convert to JSON string"""
+    """function dumps() takes an object and converts to a """
     return json.dumps(my_obj, ensure_ascii=False, indent=4)
