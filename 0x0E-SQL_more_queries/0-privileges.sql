7b7e5d3c797620a06ce774314e779ecdf96c360f
jakobo007 <oumajamesjoshua@gmail.com>
2024-05-29 10:57:18 +0300

    priviledges_2

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- script that lists all priviledges of a user in database @localhost
 -- priviledges for user_0d_1
-show grants for 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- priviledges for suer_0d_2
-show grants for 'user_0d_2'@'localhost';
+-- priviledges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
