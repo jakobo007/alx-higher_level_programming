31c7f4b207e370e0c6e2c97f1a825466013f9c21
jakobo007 <oumajames>
2024-05-29 01:37:31 -0700

    Grant select_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,24 +1,25 @@
--- script that creates database hbtn_0d_2 and user 'user_0d_2
--- user_0d_2 should have only SELECT priviledge
+-- Script that creates database hbtn_0d_2 and user 'user_0d_2'
+-- user_0d_2 should have only SELECT privilege
 -- user_0d_2 password should be user_0d_2_pwd
 -- if the database already exists the script shouldn't fail
--- if the user  already exists the script shouldn't fail
+-- if the user already exists the script shouldn't fail
 
--- create database
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
---check if the user exists
+-- Check if the user exists
 SELECT COUNT(*) INTO @count FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
 
--- create the user if it doesn't exist
-SET @create_user = IF(@count = 0,
-    CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd' ;, 'SELECT "User already exists";'   
-);
+-- Create the user if it does not exist
+SET @create_user = IF(@count = 0, 'CREATE USER \'user_0d_2\'@\'localhost\' IDENTIFIED BY \'user_0d_2_pwd\';', 'SELECT "User already exists";');
 
--- grant SELECT priviledge
-GRANT SELECT
-ON *.*
-TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+PREPARE stmt FROM @create_user;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
--- flush priviledges to ensure changes take effect:
+-- Grant SELECT privilege
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to ensure changes take effect
 FLUSH PRIVILEGES;
+
