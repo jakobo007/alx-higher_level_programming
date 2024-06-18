6b4d4f337c7826a35e003db0f926f24abf879bf4
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-06-19 00:23:39 +0300

    Constructor conditions

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+    
+    constructor(w, h) {
+        if (h <= 0 || w <= 0) {
+            Rectangle = {};
+        } else {
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
+module.exports = Rectangle;
