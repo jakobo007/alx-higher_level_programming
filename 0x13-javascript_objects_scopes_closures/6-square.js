2282bab7c025601a678fd8f83866457d5e64f357
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-06-19 01:30:21 +0300

    square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+class Square extends Square {
+    //prints rectangle using character c
+    charPrint(c) {
+        if (c === undefined) {
+            c = "X"
+        }
+        
+        for (let i = 0; i < size; i++) {
+            console.log(c.repeat(this.size));
+        }
+    }
+}
