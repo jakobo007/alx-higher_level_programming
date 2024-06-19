a0ec62692fecf92e7b1b09937b39e5a342c18659
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-06-19 10:47:40 +0300

    changes2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        //If w/h are negtive or < 0. creates empty obj automatically
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+  //If w/h are negtive or < 0. creates empty obj automatically
+  if (w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
