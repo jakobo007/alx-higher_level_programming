1c7f8cb35e8fcd40638733e064186b0baa7abed7
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-06-19 12:36:53 +0300

    er day

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 function factorial(n) {
     if (isNaN(n) || n === 0) {
-        return 1;
+        console.log(1);
     }
-
     if (n < 0) {
-        return 1;
+        console(1);
     }
-
-    return n === 1 ? 1 : n * factorial(n - 1);
+    console.log(n === 1 ? 1 : n * factorial(n - 1));
 }
