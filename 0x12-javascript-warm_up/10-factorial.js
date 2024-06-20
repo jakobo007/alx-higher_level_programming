abb92c7e8cc34c00118c6bacda1e5bc64d43c43a
jakobo007 <oumajames>
2024-06-19 18:28:30 -0700

    Late code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 function factorial(n) {
-    if (isNaN(n) || n === 0) {
-        return 1;
-    }
-
-    if (n < 0) {
-        return 1;
-    }
-
-    return n === 1 ? 1 : n * factorial(n - 1);
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  } else {
+	return n * factorial(n - 1);
+  }
 }
+
+const args = process.argv.slice(2);
+const n = parseInt(args, 10);
+console.log(factorial(n));
