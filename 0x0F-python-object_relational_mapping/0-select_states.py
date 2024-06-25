484c2c6876c7d7264b2aba7f38bdd53a9862e032
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-06-25 13:14:46 +0300

    changes_02

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""Imported Modules"""
+"""Imported modules"""
 import MySQLdb
 import sys
 
 
 def list_states(username, password, db_name):
-    """List all items in database"""
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         passwd=password, user=username, db=db_name)
+    """Lists all states from hbtn_0e_0_usa"""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+    
     cursor = db.cursor()
-    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    cursor.execute("""
+                   SELECT id, name
+                   FROM states
+                   ORDER BY id ASC""")
+    
     states = cursor.fetchall()
     
     for state in states:
-        print(f"({state[0]}, '{state[1]}')")
-
+        print(f"{state[0]}, '{state[1]}'")
+    
     cursor.close()
     db.close()
-
+    
+    
+    
 if __name__ == "__main__":
-
-
     if len(sys.argv) == 4:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        db_name = sys.argv[3]
+        username == sys.argv[1]
+        password == sys.argv[2]
+        db_name == sys.argv[3]
         list_states(username, password, db_name)
