ea315b513a82349135398b6374a32c9074801a0c
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-06-29 08:40:42 +0300

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,33 @@
-#!/usr/bin/pyhton3
-"""Imported Modules"""
+#!/usr/bin/python3
+"""script that lists all cities in a state"""
 import MySQLdb
 import sys
 
 
 def list_cities(username, password, db_name, state_name):
-    """List all cities in state"""
+    """Fucnction takes 4 args"""
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-    
     cursor = db.cursor()
     query = """
     SELECT cities.name
     FROM cities
-    JOIN states ON cities.state_id = state.id
-    WHERE states.name = %s
+    JOIN state ON cities.state_id = states.id
+    WHERE state.name = %s
     ORDER BY cities.id ASC
     """
     cursor.execute(query, (state_name,))
-    
     cities = cursor.fetchall()
-    
-    city_names = [city[0] for city in cities]
-    print(", ".join(city_names))
-    
+    for city in cities:
+        print(city)
+        
     cursor.close()
     db.close()
-    
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 5:
         username = sys.argv[1]
         password = sys.argv[2]
-        db_name= sys.argv[3]
+        db_name = sys.argv[3]
         state_name = sys.argv[4]
         list_cities(username, password, db_name, state_name)
