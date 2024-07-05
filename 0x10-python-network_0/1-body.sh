d9f9f79323847a95d4325616aeb4027c5cba233b
jakobo007 <oumajames>
2024-07-05 00:26:35 -0700

    Curl at the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# Check if the URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a GET request to the URL and display the body of the response if status code is 200
 response=$(curl -s -w "%{http_code}" "$1")
-body="${response%???}"
-status="${response: -3}"
+[ "${response: -3}" == "200" ] && echo "${response::-3}"
 
-if [ "$status" -eq 200 ]; then
-    echo "$body"
-fi
