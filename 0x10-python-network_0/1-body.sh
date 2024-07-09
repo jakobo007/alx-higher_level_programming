d4ea93a5116bfbcec36fed936a22ede0a4e0dea5
jakobo007 <oumajames>
2024-07-08 21:11:18 -0700

    cURL-end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-#script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -s -w "%{http_code}" "$1")
-[ "${response: -3}" == "200" ] && echo "${response::-3}"
+#!/usr/bin/bash
+#sends get request to url
+[ -z "$1" ] && { echo "Usage: $0 <url>"; exit 1; }
+curl -sL -w "%{http_code}" "$1" -o /tmp/response_body | grep -q 200 && cat /tmp/response_body
+
