988764ccd8fa7f891b5b65d5cd5f5ac94545b15d
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-07-10 20:55:52 +0300

    Doctest 6

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
-A function that prints My name is <first name> <last name>
+Function that prints name
 """
+
+
 def say_my_name(first_name, last_name=""):
+    """names must be strings"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     
     if last_name:
-        return f"My name is {first_name} {last_name}"
+        print(f"My name is {first_name} {last_name}")
     else:
-        return f"My name is {first_name}"
+        print(f"My name is {first_name}")
