92db477b9dbf855afbf31f720bd9bcd0330a4792
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-07-10 19:04:22 +0300

    doctest 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,8 +3,10 @@
 Module 0-add_integer
 function that adds 2 integers
 """
+
+
 def add_integer(a, b=98):
-    if not isinstance(a,(int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
