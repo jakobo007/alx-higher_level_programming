f76b4f73b9d008c133bdd13dd5e5af8133961825
jakobo007 <oumajames>
2024-07-27 03:41:15 -0700

    Network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Imported modules"""
+"""Sends a POST request to a given URL with a given email.
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
 import sys
+import urllib.parse
 import urllib.request
 
 
-def fetch_url(url, email):
-    """takes url and email, sends post request to url"""
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(url) as response:
-            result = response.read().decode('utf-8')
-            print(result)
-    
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    fetch_url(url, email)
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
