b1d02a4706eb19796925d19dbb5cc0eb9078ef91
jakobo007 <oumajames>
2024-07-27 04:05:55 -0700

    Network_4

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,7 +11,7 @@ def my_github(username, password):
     headers = {'Accept': 'application/vnd.github.v3+json'}
 
     response = requests.get(url, auth=auth, headers=headers)
- 
+
     """handle request"""
     if response.status_code == 200:
         """if successful"""
@@ -25,4 +25,3 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     my_github(username, password)
-
