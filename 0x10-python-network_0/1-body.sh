ec864461e9042d5e1bdf4a29dbb6ad310717f038
jakobo007 <oumajames>
2024-07-27 00:39:09 -0700

    Network_01

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 #sends get request to url
-[ -z "$1" ] && { echo "Usage: $0 <url>"; exit 1; }
-curl -sL -w "%{http_code}" "$1" -o /tmp/response_body | grep -q 200 && cat /tmp/response_body
-
+cur -sL "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
- # Send a POST request to the URL with the variables email and subject
-curl -s -
-X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+# Send a POST request to the URL with the variables email and subject
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 
