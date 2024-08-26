268556011296bc38785b586baeacac030768cb18
Ouma James Joshua <oumajamesjoshua@gmail.com>
2024-08-26 14:44:39 +0300

    Changes 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-//Read and prints file
 
-const fs = require('fs')
-const FilePath = process.argv[2]
+const fs = require('fs');
+const FilePath = process.argv[2];
 
 fs.readFile(FilePath, 'utf-8', (err, data) => {
     if (err) {
